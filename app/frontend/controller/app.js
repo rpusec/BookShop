f8e885cd23830ed8081cb1ef872db202cdafa315
Roman Pusec <roman.pusec@gmail.com>
2016-06-17 17:56:20 +0200

    Added routing.

--- app/frontend/controller/app.js
+++ app/frontend/controller/app.js
@@ -1,5 +1,14 @@
-var app = angular.module("myApp", ['ui.bootstrap', 'services']);
+var app = angular.module("myApp", ['ui.bootstrap', 'ui.router', 'services']);
 
 app.run(function($rootScope){
 	$rootScope.navbarUrl = 'app/frontend/includes/navbar.html';
+});
+
+app.config(function($stateProvider, $urlRouterProvider, $locationProvider){
+	$urlRouterProvider.otherwise('/books');
+	$stateProvider
+		.state('books', {url: '/books', templateUrl: 'app/frontend/includes/pages/users.html'})	
+		.state('cart', {url: '/cart', templateUrl: 'app/frontend/includes/pages/cart.html'})	
+		.state('admin', {url: '/admin', templateUrl: 'app/frontend/includes/pages/admin.html'})	
+		.state('logout', {url: '/logout', templateUrl: 'app/frontend/includes/pages/logout.html'})	
 });
--- app/frontend/controller/navbarContr.js
+++ app/frontend/controller/navbarContr.js
@@ -1,10 +1,28 @@
 app.controller('navbarContr', function($scope){
 	$scope.appTitle = 'BookShop';
-	$scope.navLinks = [
-		{title: 'Home', active: 'active'},
-		{title: 'Books'},
-		{title: 'Cart'},
-		{title: 'Users'},
-		{title: 'Logout'}
+	$scope.arrNavLeft = [
+		{title: 'Books', uri: 'books', active: 'active'},
+		{title: 'Cart', uri: 'cart'},
+		{title: 'Admin', uri: 'admin'}
 	];
+	$scope.arrNavRight = [
+		{title: 'Logout', uri: 'logout'}
+	];
+
+	$scope.highlightTitle = function(title, isNavLeft){
+		angular.forEach(isNavLeft ? this.arrNavLeft : this.arrNavRight, function(link){
+			if(link.title === title)
+				link.active = 'active';
+			else if(link.active === 'active')
+				link.active = null;
+		});
+
+		angular.forEach(!isNavLeft ? this.arrNavLeft : this.arrNavRight, function(link){
+			if(link.active === 'active')
+			{
+				link.active = null;
+				return false;
+			}
+		});
+	}
 });
